e6fcc44e2024e2dd8b110a6fd06f3d82ade82934
BUzunovic <buzunovic1@etf.unsa.ba>
2025-04-16 16:35:25 +0200

    Create branch VIsualSeatSelection

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import AdminHomePage from './pages/AdminHomePage/AdminHomePage';
 import Profile from './pages/Profile/Profile';
 import FlightSearch from './pages/FlightSearch/FlightSearch';
 import FlightScheduling from './pages/FlightScheduling/FlightScheduling';
+import { VisualSeatSelection } from './pages';
 
 function App() {
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,2 @@
 export {default as FlightSearch} from './FlightSearch/FlightSearch.jsx'
+export {default as VisualSeatSelection} from './VisualSeatSelection/VisualSeatSelection.jsx'
